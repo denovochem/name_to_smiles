b6e04cec0be60842e4a0a44e0a1155ead50b6bb4
csnbritt <carson.britt@cssquaredllc.com>
2025-12-09 21:16:51 -0700

    Update __init__.py to import additional resolvers and rename main function
    Changed import from convert_name_to_smiles to resolve_compounds_to_smiles and added imports for ManualNameResolver, OpsinNameResolver, PubChemNameResolver, PeptideNameResolver, StructuralFormulaNameResolver, and CIRPyNameResolver from main module.

--- placeholder_name/__init__.py
+++ placeholder_name/__init__.py
@@ -1,5 +1,5 @@
 """name_to_smiles initialization."""
-from .main import convert_name_to_smiles
+from .main import resolve_compounds_to_smiles, ManualNameResolver, OpsinNameResolver, PubChemNameResolver, PeptideNameResolver, StructuralFormulaNameResolver, CIRPyNameResolver
 
 __all__ = [
   'resolve_compounds_to_smiles',
